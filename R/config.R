37ff1b07ffa10acd0c43fa5b75b2a955c9b13559
davidruvolo51 <davidruvolo51@gmail.com>
2020-10-29 21:16:15 +0100

    update config methods and pkg metadata (#2)

--- R/config.R
+++ R/config.R
@@ -3,37 +3,50 @@
 #' Methods for validating and managing the configuration file
 #'
 #' @noRd
-pbconfig <- structure(list(), class = "config")
+pbconfig <- structure(list(), version = "0.0.3", class = "config")
 
+#' pkgbump config filename
+#'
+#' @noRd
+pbconfig$filename <- "pkgbump.config.json"
 
 #' New Pkgbump Config Template
 #'
-#' @param version string containing the pkgbump package's version number
 #' @param files array containing file paths to ignore
 #'
 #' @noRd
-pbconfig$new_config <- function(version, files) {
+pbconfig$new_config <- function(files) {
     list(
-        name = "pkgbump",
-        version = version,
-        description = "config file for version number management",
+        name = "pkgbump.config.json",
+        version = attr(pbconfig, "version"),
+        description = "pkgbump config file",
         timestamps = list(
             created = Sys.time(),
             updated = Sys.time()
         ),
         config = list(
             files = length(files),
-            paths = files
+            paths = files,
+            patterns = list(
+                R = "version = .\\d+.\\d+.\\d+.|version=.\\d+.\\d+.\\d+.",
+                DESCRIPTION = "Version:"
+            )
+        ),
+        dependencies = list(
+            cli = "^2.1.0",
+            jsonlite = "^1.7.1",
+            stringr = "^1.4.0"
+        ),
+        license = "MIT",
+        author = "@dcruvolo",
+        bugs = list(
+           url = "https://github.com/davidruvolo51/pkgbump/issues"
         )
     )
 }
 
 
-#' Add to Rbuildignore
-#'
-#' Add pkgbump filename to .Rbuildignore file
-#'
-#' @param pattern pattern to ignore
+#' Add pkgbump config filename to Rbuildignore
 #'
 #' @noRd
 pbconfig$ignore_config <- function(pattern = "^\\.pkgbump\\.json$") {
@@ -46,9 +59,13 @@ pbconfig$ignore_config <- function(pattern = "^\\.pkgbump\\.json$") {
                 "Added {.val {pattern}} to {.file .Rbuildignore}"
             )
         }, error = function(e) {
-            cli::cli_alert_danger("Failed to update {.file .Rbuildignore: {e}")
+            cli::cli_alert_danger(
+                "Failed to update {.file .Rbuildignore: \n {e}"
+            )
         }, warning = function(w) {
-            cli::cli_alert_danger("Failed to update {.file .Rbuildignore: {w}")
+            cli::cli_alert_danger(
+                "Failed to update {.file .Rbuildignore: \n {w}"
+            )
         })
     }
     if (length(r[r == pattern]) > 0) {

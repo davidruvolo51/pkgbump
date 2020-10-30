c05063455d66541997ea22ec21d10e6f96869705
davidruvolo51 <davidruvolo51@gmail.com>
2020-10-30 19:30:16 +0100

    added auto config update and error handling (#2)

--- R/set_pkgbump.R
+++ R/set_pkgbump.R
@@ -11,57 +11,73 @@
 #'
 #' @examples
 #' \dontrun{
-#'   set_pkgbump(files = c("package.json", "DESCRIPTION"))
+#' set_pkgbump(files = c("package.json", "DESCRIPTION"))
 #' }
 #'
 #' @export
 set_pkgbump <- function(files, r_patterns = NULL) {
     f <- pbconfig$filename
-    status <- file.exists(f)
-
-    if (status) {
-        c <- jsonlite::read_json(f)
-        cli::cli_alert_success("Updated {.pkg pkgbump} configuration")
-        c$timestamps$updated <- Sys.time()
-        c$config$files <- length(files)
-        c$config$paths <- files
+    should_quit <- f %in% files
+    if (should_quit) {
+        cli::cli_alert_danger("Files cannot have {.file {f}}")
     }
 
-    if (!status) {
-        cli::cli_alert_success("Created {.file {f}}")
-        file.create(f)
-        c <- pbconfig$new_config(files = files)
-    }
+    if (!should_quit) {
+        status <- file.exists(f)
 
-    # append to buildignore if present
-    if (file.exists(".Rbuildignore")) {
-        pbconfig$ignore_config()
-    }
-
-
-    # append R patterns if applicable
-    if (!is.null(r_patterns)) {
-        if (!is.character(r_patterns)) {
-            cli::cli_alert_danger("{.arg patterns} must be a string")
-        } else if (length(r_patterns) > 1) {
-            cli::cli_alert_danger("{.arg patterns} must be a string")
-        } else {
-            c$patterns$R <- r_patterns
+        if (status) {
+            c <- jsonlite::read_json(f)
+            if (c$version == attr(pbconfig, "version")) {
+                c$timestamps$updated <- Sys.time()
+                c$config$files <- length(files)
+                c$config$paths <- files
+            }
+            if (c$version != attr(pbconfig, "version")) {
+                cli::cli_alert_info("Updating {.file {f}} to latest version")
+                c <- pbconfig$new_config(files = files)
+            }
         }
-    }
 
-    # save and confirm
-    tryCatch({
-        jsonlite::write_json(
-            x = c,
-            path = f,
-            pretty = TRUE,
-            auto_unbox = TRUE
+        if (!status) {
+            cli::cli_alert_success("Created {.file {f}}")
+            file.create(f)
+            c <- pbconfig$new_config(files = files)
+        }
+
+        # append to buildignore if present
+        if (file.exists(".Rbuildignore")) {
+            pbconfig$ignore_config()
+        }
+
+
+        # append R patterns if applicable
+        if (!is.null(r_patterns)) {
+            if (!is.character(r_patterns)) {
+                cli::cli_alert_danger("{.arg patterns} must be a string")
+            } else if (length(r_patterns) > 1) {
+                cli::cli_alert_danger("{.arg patterns} must be a string")
+            } else {
+                c$patterns$R <- r_patterns
+            }
+        }
+
+        # save and confirm
+        tryCatch(
+            {
+                jsonlite::write_json(
+                    x = c,
+                    path = f,
+                    pretty = TRUE,
+                    auto_unbox = TRUE
+                )
+                cli::cli_alert_success("Updated {.file {f}}")
+            },
+            error = function(e) {
+                cli::cli_alert_danger("Failed to update {.file {f}}: {e}")
+            },
+            warning = function(w) {
+                cli::cli_alert_danger("Failed to update {.file {f}}: {w}")
+            }
         )
-        cli::cli_alert_success("Saved {.file {f}}")
-    }, error = function(e) {
-        cli::cli_alert_danger("Failed to update {.file {f}}: {e}")
-    }, warning = function(w) {
-        cli::cli_alert_danger("Failed to update {.file {f}}: {w}")
-    })
+    }
 }
